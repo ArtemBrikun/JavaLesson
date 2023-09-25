47bc0cb9b0516112f0bfa86477b2ce1e8cf55da4
ArtemBrikun <brikunseergeevic210599@gmail.com>
2023-09-25 18:37:55 +0300

    Swap two variables.

--- src/Homework/HW1/Main.java
+++ src/Homework/HW1/Main.java
@@ -5,13 +5,7 @@ package Homework.HW1;
 public class Main {
     public static void main(String[] args) {
 
-        String name = "Artem";
-        int growth = 175;
-        float weight = 70.5f;
-
-        System.out.println("Name=" + name + ", Growth=" + growth + ", Weight=" + weight);
 
 
+        }
     }
-
-}
