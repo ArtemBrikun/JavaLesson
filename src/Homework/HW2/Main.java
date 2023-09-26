1165e8bb40c47766bc8de59e2737b05c400b4582
ArtemBrikun <brikunseergeevic210599@gmail.com>
2023-09-27 01:30:24 +0300

    Create an array of fractional numbers.

--- src/Homework/HW2/Main.java
+++ src/Homework/HW2/Main.java
@@ -5,6 +5,12 @@ package Homework.HW2;
 public class Main {
     public static void main(String[] args) {
 
+        double[] doubleArray = {1.2, 3.4, 5.6, 7.8, 9.0};
+
+
+        for (int i = 0; i < doubleArray.length; i++) {
+            System.out.println(" " + i + ": " + doubleArray[i]);
+        }
 
 
     }
