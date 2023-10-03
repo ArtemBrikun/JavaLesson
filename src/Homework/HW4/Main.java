35b5317b32e0b0608a58f71ae99d4a553809681f
ArtemBrikun <brikunseergeevic210599@gmail.com>
2023-10-03 18:26:00 +0300

    Display the maximum and minimum elements of the array on the screen

--- src/Homework/HW4/Main.java
+++ src/Homework/HW4/Main.java
@@ -1,4 +1,35 @@
 package Homework.HW4;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = new int[10];
+
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < 10; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+
+
+    }
 }
